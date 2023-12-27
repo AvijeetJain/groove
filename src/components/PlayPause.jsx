74919ca5cd6460cb6d20da4dfb9ce46bf16b770b
AvijeetJain <jainavijeet@gmail.com>
2023-12-28 01:12:00 +0530

    Condition Local -> Universal

--- src/components/PlayPause.jsx
+++ src/components/PlayPause.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import { FaPauseCircle, FaPlayCircle } from 'react-icons/fa';
 
 const PlayPause = ({song, handlePause, handlePlay, isPlaying, activeSong}) => 
-( isPlaying && activeSong?.title === song.song.title ? (
+( isPlaying && activeSong?.title === song.title ? (
     <FaPauseCircle 
-      size={35}
-      className="text-gray-300"
-      onClick={handlePause}
+      size = {35}
+      className = "text-gray-300"
+      onClick = {handlePause}
     />
 ) : (
     <FaPlayCircle 
-      size={35}
-      className="text-gray-300"
-      onClick={handlePlay}
+      size = {35}
+      className = "text-gray-300"
+      onClick = {handlePlay}
     />
 )
 );
