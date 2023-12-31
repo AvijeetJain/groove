dcefbe35e2a8bed935f1a4b401ad568f9a922334
AvijeetJain <jainavijeet@gmail.com>
2023-12-31 21:00:04 +0530

    TopChartsEdited/AddedMoreSongs"
    q

--- src/pages/TopCharts.jsx
+++ src/pages/TopCharts.jsx
@@ -1,16 +1,16 @@
 import { useSelector } from "react-redux";
 import { Error, Loader, SongCard } from "../components";
 import { useGetChartsQuery } from "../redux/services/shazamCore";
+import { useGetCharts2Query } from "../redux/services/shazamCore";
 
 const TopCharts = () => {
     const { activeSong, isPlaying } = useSelector((state) => state.player);  
     const { data, isFetching, error } = useGetChartsQuery();
+    const { data: data2, isFetching: isFetchingMore, error: errorMore } = useGetCharts2Query();
 
-    const allSongs = data?.tracks;
-
-    if(isFetching) return <Loader title="Loading Songs ... "/>;
-    if(error) return <Error title="Something went wrong" />;
-
+    if(isFetching || isFetchingMore) return <Loader title="Loading Songs ... "/>;
+    if(error && errorMore) return <Error title="Something went wrong" />;
+    
     return (
         <div className="flex flex-col">
             <div className="w-full flex justify-between items-center sm:flex-row mt-4 mb-10"> 
@@ -21,7 +21,19 @@ const TopCharts = () => {
 
             <div className="flex flex-wrap sm:justify-start justify-center gap-8">
                 {
-                    allSongs?.map((song, i) => (
+                    data?.tracks?.map((song, i) => (
+                        <SongCard 
+                            key = {song.key}
+                            song = {song}
+                            isPlaying = {isPlaying}
+                            activeSong = {activeSong}
+                            data = {data.tracks}
+                            i = {i}
+                        />
+                    ))
+                }
+                {
+                    data2?.tracks?.map((song, i) => (
                         <SongCard 
                             key = {song.key}
                             song = {song}
